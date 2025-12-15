f0dd6f32ebd85f11ad9e43aecac5000906938814
hayatroid <hayatroid@gmail.com>
2025-12-15 02:04:53 +0000

    trait DFA, NFA

---
+++ src/nfa.rs
@@ -0,0 +1,7 @@
+pub trait NFA {
+    type Q;
+    type A;
+    fn q_init(&self) -> Self::Q;
+    fn q_next(&self, q: &Self::Q, a: Option<&Self::A>) -> Vec<Self::Q>;
+    fn is_q_accept(&self, q: &Self::Q) -> bool;
+}
